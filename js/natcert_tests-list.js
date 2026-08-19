174fbdff5057c1a3935f776fe0a1ae78eff8fdcb
Isfan <isko66066@gmail.com>
2026-08-19 11:33:40 +0500

    ,

--- js/natcert_tests-list.js
+++ js/natcert_tests-list.js
@@ -1,5 +1,6 @@
 var testsList = [
     { id: 1, title: "28-февраля, 1 смена", questionsCount: 45, year: '2026' },
     { id: 2, title: "28-февраля, 2 смена", questionsCount: 45, year: '2026' },
-    { id: 3, title: "1-марта, 1 смена", questionsCount: 45, year: '2026' }
+    { id: 3, title: "1-марта, 1 смена", questionsCount: 45, year: '2026' },
+    { id: 4, title: "1-марта, 2 смена", questionsCount: 45, year: '2026' }
 ];
