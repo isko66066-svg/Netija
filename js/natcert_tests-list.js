fcf3654e960d5960121ade7c5d5cf4bae51423c1
Isfan <isko66066@gmail.com>
2026-08-20 19:45:11 +0500

    Добавить тест 2 марта в список тестов

--- js/natcert_tests-list.js
+++ js/natcert_tests-list.js
@@ -2,5 +2,6 @@ var testsList = [
     { id: 1, title: "28-февраля, 1 смена", questionsCount: 45, year: '2026' },
     { id: 2, title: "28-февраля, 2 смена", questionsCount: 45, year: '2026' },
     { id: 3, title: "1-марта, 1 смена", questionsCount: 45, year: '2026' },
-    { id: 4, title: "1-марта, 2 смена", questionsCount: 45, year: '2026' }
+    { id: 4, title: "1-марта, 2 смена", questionsCount: 45, year: '2026' },
+    { id: 5, title: "2-марта, 1 смена", questionsCount: 45, year: '2026' }
 ];
