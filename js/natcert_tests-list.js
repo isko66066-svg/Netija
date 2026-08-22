c61ec614379a1961bc33a976c0749659c52cf281
Isfan <isko66066@gmail.com>
2026-08-22 15:46:31 +0500

    Добавить тесты 23.02.2025 в список

--- js/natcert_tests-list.js
+++ js/natcert_tests-list.js
@@ -6,5 +6,7 @@ var testsList = [
     { id: 5, title: "2-марта, 1 смена", questionsCount: 45, year: '2026' },
     { id: 6, title: "2-марта, 2 смена", questionsCount: 45, year: '2026' },
     { id: 7, title: "7-марта, 1 смена", questionsCount: 45, year: '2026' },
-    { id: 8, title: "7-марта, 2 смена", questionsCount: 45, year: '2026' }
+    { id: 8, title: "7-марта, 2 смена", questionsCount: 45, year: '2026' },
+    { id: 9, title: "23 февраля 2025, 1 вариант", questionsCount: 45, year: '2025' },
+    { id: 10, title: "23 февраля 2025, 2 вариант", questionsCount: 45, year: '2025' }
 ];
