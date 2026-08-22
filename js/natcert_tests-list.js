bc12a4e4c9ae1bc34f346ed0a3d2d04f2982d8da
Isfan <isko66066@gmail.com>
2026-08-22 18:19:26 +0500

    Add 25 May 2025 tests to test list and format list

--- js/natcert_tests-list.js
+++ js/natcert_tests-list.js
@@ -1,12 +1,74 @@
-var testsList = [
-    { id: 1, title: "28-февраля 2026, 1 смена", questionsCount: 45, year: '2026' },
-    { id: 2, title: "28-февраля 2026, 2 смена", questionsCount: 45, year: '2026' },
-    { id: 3, title: "1-марта 2026, 1 смена", questionsCount: 45, year: '2026' },
-    { id: 4, title: "1-марта 2026, 2 смена", questionsCount: 45, year: '2026' },
-    { id: 5, title: "2-марта 2026, 1 смена", questionsCount: 45, year: '2026' },
-    { id: 6, title: "2-марта 2026, 2 смена", questionsCount: 45, year: '2026' },
-    { id: 7, title: "7-марта 2026, 1 смена", questionsCount: 45, year: '2026' },
-    { id: 8, title: "7-марта 2026, 2 смена", questionsCount: 45, year: '2026' },
-    { id: 9, title: "23 февраля 2025, 1 смена", questionsCount: 45, year: '2025' },
-    { id: 10, title: "23 февраля 2025, 2 смена", questionsCount: 45, year: '2025' }
+const testsList = [
+    {
+        id: 1,
+        title: "28-февраля 2026, 1 смена",
+        questionsCount: 45,
+        year: "2026"
+    },
+    {
+        id: 2,
+        title: "28-февраля 2026, 2 смена",
+        questionsCount: 45,
+        year: "2026"
+    },
+    {
+        id: 3,
+        title: "1-марта 2026, 1 смена",
+        questionsCount: 45,
+        year: "2026"
+    },
+    {
+        id: 4,
+        title: "1-марта 2026, 2 смена",
+        questionsCount: 45,
+        year: "2026"
+    },
+    {
+        id: 5,
+        title: "2-марта 2026, 1 смена",
+        questionsCount: 45,
+        year: "2026"
+    },
+    {
+        id: 6,
+        title: "2-марта 2026, 2 смена",
+        questionsCount: 45,
+        year: "2026"
+    },
+    {
+        id: 7,
+        title: "7-марта 2026, 1 смена",
+        questionsCount: 45,
+        year: "2026"
+    },
+    {
+        id: 8,
+        title: "7-марта 2026, 2 смена",
+        questionsCount: 45,
+        year: "2026"
+    },
+    {
+        id: 9,
+        title: "23 февраля 2025, 1 смена",
+        questionsCount: 45,
+        year: "2025"
+    },
+    {
+        id: 10,
+        title: "23 февраля 2025, 2 смена",
+        questionsCount: 45,
+        year: "2025"
+    },
+    {
+        id: 11,
+        title: "25 мая 2025, 1 смена",
+        questionsCount: 45,
+        year: "2025"
+    },
+    {
+        id: 12,
+        title: "25 мая 2025, 2 смена",
+        questionsCount: 45,
+        year: "2025"
+    }
 ];
